a8375e6a3172779c62a1ae72e21fb947daeab992
Pasi Junttila <pasi.junttila@live.com>
2018-09-26 06:38:48 +0300

    Tehtava 5.1 blogilistan frontend, osa 1: kirjautuminen, ehdollinen rendaus.

--- frontend5/src/App.js
+++ frontend5/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import Loginform from './components/Loginform'
+import LoginService from './services/login'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      blogs: []
+      blogs: [],
+      user: null
     }
   }
 
@@ -17,15 +20,39 @@ class App extends React.Component {
   }
 
   render() {
+    const blogList = this.state.blogs.map(blog =>
+      <Blog key={blog.id} blog={blog}/>
+    );
     return (
       <div>
-        <h2>blogs</h2>
-        {this.state.blogs.map(blog =>
-          <Blog key={blog.id} blog={blog}/>
-        )}
+        { this.state.user === null &&
+          <Loginform
+            onUserLogin={this.loginPost}
+          />
+        }
+        {this.state.user !== null &&
+          <div>
+            <h2>blogs</h2>
+            {this.state.user.name} logged in<br /><br />
+            {blogList}
+          </div>
+        }
       </div>
     );
   }
+
+  loginPost = async (username, password) => {
+    try {
+      const result = await LoginService.login(username, password);
+      this.setState({user: result});
+    }
+    catch (error) {
+      if (error.response.data.error) {
+        console.log("FAIL:", error.response.data.error);
+      }
+    }
+  }
+
 }
 
 export default App;
