fd831edb822659f7b7bf5f0ef84b2e4cab094468
Pasi Junttila <pasi.junttila@live.com>
2018-09-27 19:10:13 +0300

    Tehtava 5.18 unicafe revisited, osa 2: sovelluksen toiminnallisuus.

--- unicafe-redux/src/index.js
+++ unicafe-redux/src/index.js
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom';
 import {createStore} from 'redux'
 import noteReducer from './noteReducer'
 
+const btnStyle = {
+  borderRadius: 5,
+  padding: 8,
+  cursor: 'pointer',
+  borderWidth: 1,
+  margin: 2,
+  background: 'white'
+};
+
 const store = createStore(noteReducer);
 
 const Statistiikka = ({inputs}) => {
-  let palautteita = 0
-    if (palautteita === 0) {
+  let palautteita = 0;
+  for (let attr in inputs) {
+    palautteita += inputs[attr];
+  }
+  let average = ((1 * inputs.good) + (-1 * inputs.bad)) / (1.0 * palautteita)
+  let positive = inputs.good/(palautteita * 1.0) * 100
+  if (palautteita === 0) {
     return (
       <div>
         <h2>statistiikka</h2>
@@ -15,7 +29,7 @@ const Statistiikka = ({inputs}) => {
       </div>
     )
   }
-
+  const avgStyle = average >= 0 ? {} : {background: '#ff4444', color: 'white'};
   return (
     <div>
       <h2>statistiikka</h2>
@@ -23,35 +37,35 @@ const Statistiikka = ({inputs}) => {
         <tbody>
           <tr>
             <td>hyvä</td>
-            <td></td>
+            <td>{inputs.good}</td>
           </tr>
           <tr>
             <td>neutraali</td>
-            <td></td>
+            <td>{inputs.ok}</td>
           </tr>
           <tr>
             <td>huono</td>
-            <td></td>
+            <td>{inputs.bad}</td>
           </tr>
           <tr>
             <td>keskiarvo</td>
-            <td></td>
+            <td style={avgStyle}>{average.toFixed(2)}</td>
           </tr>
           <tr>
             <td>positiivisia</td>
-            <td></td>
+            <td>{positive.toFixed(2)} %</td>
           </tr>
         </tbody>
       </table>
 
-      <button>nollaa tilasto</button>
+      <button style={btnStyle} onClick={()=>store.dispatch({type: 'CLEAR'})}>nollaa tilasto</button>
     </div>
   )
 }
 
 class App extends React.Component {
   klik = (nappi) => {
-
+    store.dispatch({ type: nappi});
   }
 
   render() {
@@ -60,9 +74,9 @@ class App extends React.Component {
     return (
       <div>
         <h2>anna palautetta</h2>
-        <button onClick={() => this.klik('GOOD')}>hyvä</button>
-        <button onClick={() => this.klik('OK')}>neutraali</button>
-        <button onClick={() => this.klik('BAD')}>huono</button>
+        <button style={btnStyle} onClick={() => this.klik('GOOD')}>hyvä</button>
+        <button style={btnStyle} onClick={() => this.klik('OK')}>neutraali</button>
+        <button style={btnStyle} onClick={() => this.klik('BAD')}>huono</button>
         <Statistiikka inputs={inputs}/>
 
       </div>
